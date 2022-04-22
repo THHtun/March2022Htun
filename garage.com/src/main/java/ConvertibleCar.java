09b15c2a32bd4e21afafc7297dea60b538f78a91
thein.htun <thein.htun@fdmgroup.com>
2022-04-22 15:44:38 -0700

    c

--- garage.com/src/main/java/ConvertibleCar.java
+++ garage.com/src/main/java/ConvertibleCar.java
@@ -1,6 +1,26 @@
 public class ConvertibleCar extends Car implements ICar {
-
+	private boolean topDown;
+	private boolean topRaise;
 	public ConvertibleCar(String model, IEngine engine, IIgnition ignitionSystem) {
 		super(model, engine, ignitionSystem);
+	
+		}
+
+	
+
+	public boolean getToplowered()
+	{
+		this.topDown = true;
+		return this.topDown;
 	}
+
+	public boolean getTopRaised()
+	{
+		this.topRaise = true;
+		return this.topRaise;
+	}
+
+	
+	
+
 }
